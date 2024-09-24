c0f154404d0c42688205d6349e1dd171bb85a9ba
CaioDamascenoAlves <caio.damasceno@aluno.ufop.edu.br>
2024-09-24 13:46:55 -0300

    Voltando Versão do Componente Exp's

--- src/components/Jobs/ExperienceCard.jsx
+++ src/components/Jobs/ExperienceCard.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from "react";
+import React from "react";
 import {
   Box,
   Card,
@@ -7,7 +7,6 @@ import {
   Chip,
   useTheme,
 } from "@mui/material";
-import { motion } from "framer-motion";
 
 const experiences = [
   {
@@ -30,43 +29,11 @@ const experiences = [
     company: "Grupo Belmont",
     position: "Auxiliar Administrativo",
     duration: "Janeiro de 2017 - agosto de 2018 (1 ano e 6 meses)",
-  },
+  }
 ];
 
 const ExperienceCard = () => {
   const theme = useTheme();
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const cardRef = useRef(null);
-
-  const handleScroll = (event) => {
-    if (event.deltaY > 0) {
-      // Scroll para baixo
-      setCurrentIndex((prevIndex) => {
-        const nextIndex = (prevIndex + 1) % experiences.length;
-        return nextIndex;
-      });
-    } else {
-      // Scroll para cima
-      setCurrentIndex((prev) => {
-        const newIndex = (prev - 1 + experiences.length) % experiences.length;
-        return newIndex;
-      });
-    }
-  };
-
-  useEffect(() => {
-    const currentCard = cardRef.current;
-
-    const handleWheel = (event) => {
-      event.preventDefault(); // Evita o comportamento padrão do scroll
-      handleScroll(event);
-    };
-
-    currentCard.addEventListener("wheel", handleWheel);
-    return () => {
-      currentCard.removeEventListener("wheel", handleWheel);
-    };
-  }, []);
 
   return (
     <Box
@@ -79,7 +46,6 @@ const ExperienceCard = () => {
         backgroundColor: theme.palette.background.default,
         borderTop: `1px solid ${theme.palette.divider}`,
       }}
-      ref={cardRef}
     >
       <Typography
         variant="h4"
@@ -91,50 +57,45 @@ const ExperienceCard = () => {
         Experiências
       </Typography>
 
-      <Box sx={{ width: "100%", maxWidth: 600 }}>
-        <motion.div
-          key={currentIndex}
-          initial={{ opacity: 0, y: -50 }}
-          animate={{ opacity: 1, y: 0 }}
-          exit={{ opacity: 0, y: 50 }}
-          transition={{ duration: 0.5 }}
+      {experiences.map((exp, index) => (
+        <Card
+          key={index}
+          sx={{
+            width: "100%",
+            maxWidth: 600,
+            backgroundColor: theme.palette.background.paper,
+            boxShadow: theme.shadows[3],
+            borderRadius: "12px",
+            border: `1px solid ${theme.palette.primary.main}`,
+          }}
         >
-          <Card
-            sx={{
-              backgroundColor: theme.palette.background.paper,
-              boxShadow: theme.shadows[3],
-              borderRadius: "12px",
-              border: `1px solid ${theme.palette.primary.main}`,
-            }}
-          >
-            <CardContent>
-              <Box sx={{ display: "flex", flexDirection: "column", gap: 1 }}>
-                <Chip
-                  label={experiences[currentIndex].company}
-                  sx={{
-                    alignSelf: "start",
-                    backgroundColor: theme.palette.primary.main,
-                    color: theme.palette.primary.contrastText,
-                    fontWeight: "bold",
-                  }}
-                />
-                <Typography
-                  variant="h6"
-                  sx={{ color: theme.palette.text.primary }}
-                >
-                  {experiences[currentIndex].position}
-                </Typography>
-                <Typography
-                  variant="body2"
-                  sx={{ color: theme.palette.text.secondary }}
-                >
-                  {experiences[currentIndex].duration}
-                </Typography>
-              </Box>
-            </CardContent>
-          </Card>
-        </motion.div>
-      </Box>
+          <CardContent>
+            <Box sx={{ display: "flex", flexDirection: "column", gap: 1 }}>
+              <Chip
+                label={exp.company}
+                sx={{
+                  alignSelf: "start",
+                  backgroundColor: theme.palette.primary.main,
+                  color: theme.palette.primary.contrastText,
+                  fontWeight: "bold",
+                }}
+              />
+              <Typography
+                variant="h6"
+                sx={{ color: theme.palette.text.primary }}
+              >
+                {exp.position}
+              </Typography>
+              <Typography
+                variant="body2"
+                sx={{ color: theme.palette.text.secondary }}
+              >
+                {exp.duration}
+              </Typography>
+            </Box>
+          </CardContent>
+        </Card>
+      ))}
     </Box>
   );
 };
