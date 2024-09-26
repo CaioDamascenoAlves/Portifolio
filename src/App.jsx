8a8c4f4b61ff187ae32f0a7da76cb103fb9cbd98
CaioDamascenoAlves <caio.damasceno@aluno.ufop.edu.br>
2024-09-25 23:21:16 -0300

    Adicionando Rota e anamação

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { Suspense, lazy, useEffect, useState } from "react";
 import { Box } from "@mui/material";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 const ThemeSwitcher = lazy(() => import("./components/Theme/ThemeSwitcher"));
@@ -9,9 +10,12 @@ const About = lazy(() => import("./components/Sobre/Sobre"));
 const Habilidades = lazy(() => import("./components/Skills/Skills"));
 const Projects = lazy(() => import("./components/Projects/Projects"));
 const Contact = lazy(() => import("./components/Contact/ContactMe"));
-const LoadingWalking = lazy(() => import("./components/Animation/LottiWalking"));
+const LoadingWalking = lazy(() =>
+  import("./components/Animation/LottiWalking")
+);
 const ScrollToTopButton = lazy(() => import("./components/ScrollToTopButton"));
 const ExperienceCard = lazy(() => import("./components/Jobs/ExperienceCard"));
+const LottiCode = lazy(() => import("./components/Animation/LottiCode")); // Nova rota para animação
 
 function App() {
   const [showScrollButton, setShowScrollButton] = useState(false);
@@ -22,7 +26,6 @@ function App() {
 
     const sectionBottom = homeSection.getBoundingClientRect().bottom;
 
-    // Exibe o botão se o topo da seção Home estiver acima da tela
     if (sectionBottom < 0) {
       setShowScrollButton(true);
     } else {
@@ -40,42 +43,53 @@ function App() {
   return (
     <Suspense fallback={<LoadingWalking />}>
       <ThemeSwitcher>
-        <Box
-          sx={{
-            bgcolor: "background.default",
-            minHeight: "100vh",
-            color: "text.primary",
-          }}
-        >
-          <Sidebar />
+        <Router basename="/Portifolio">
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <Box
+                  sx={{
+                    bgcolor: "background.default",
+                    minHeight: "100vh",
+                    color: "text.primary",
+                  }}
+                >
+                  <Sidebar />
 
-          <section id="home">
-            <CaioProfile />
-          </section>
+                  <section id="home">
+                    <CaioProfile />
+                  </section>
 
-          <section id="sobre">
-            <About />
-          </section>
+                  <section id="sobre">
+                    <About />
+                  </section>
 
-          <section id="habilidades">
-            <Habilidades />
-          </section>
+                  <section id="habilidades">
+                    <Habilidades />
+                  </section>
 
-          <section id="projetos">
-            <Projects />
-          </section>
+                  <section id="projetos">
+                    <Projects />
+                  </section>
 
-          <section id="experiencias">
-            <ExperienceCard />
-          </section>
+                  <section id="experiencias">
+                    <ExperienceCard />
+                  </section>
 
-          <section id="contato">
-            <Contact />
-          </section>
+                  <section id="contato">
+                    <Contact />
+                  </section>
 
-          {/* Renderiza o botão apenas se showScrollButton for true */}
-          {showScrollButton && <ScrollToTopButton />}
-        </Box>
+                  {showScrollButton && <ScrollToTopButton />}
+                </Box>
+              }
+            />
+
+            {/* Nova rota para a página de animação */}
+            <Route path="/animacao" element={<LottiCode />} />
+          </Routes>
+        </Router>
       </ThemeSwitcher>
     </Suspense>
   );
---
+++ src/components/Animation/LottiCode.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import Lottie from 'react-lottie-player';
+import animationData from "../../assets/lottie/code.json";
+
+const LoadingSpinner = () => {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+      }}
+    >
+      <Lottie
+        loop
+        animationData={animationData}
+        play
+        style={{ width: 200, height: 200 }}
+        // Ajuste o framerate se necessário
+        framerate={30}
+      />
+    </Box>
+  );
+};
+
+export default React.memo(LoadingSpinner);
